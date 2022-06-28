e7591c8948aff3f9da6e2d2f51c9e8298ac765a5
Micaela Acuña <micahl1981@gmail.com>
2022-06-28 11:07:47 -0300

    reporte para azure-pipelines

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,27 @@
-module.exports = function (config) {
-    "use strict";
+// module.exports = function (config) {
+//     "use strict";
 
+//     config.set({
+//         frameworks: ["ui5"],
+//         ui5: {
+//             type: "application",
+//             configPath: "uimodule/ui5.yaml",
+//             paths: {
+//                 webapp: "uimodule/webapp"
+//             }
+//         },
+//         browsers: ["Chrome"],
+//         browserConsoleLogOptions: {
+//             level: "error"
+//         }
+//     });
+// };
+
+module.exports = function (config) {
     config.set({
+
         frameworks: ["ui5"],
+
         ui5: {
             type: "application",
             configPath: "uimodule/ui5.yaml",
@@ -10,9 +29,25 @@ module.exports = function (config) {
                 webapp: "uimodule/webapp"
             }
         },
-        browsers: ["Chrome"],
-        browserConsoleLogOptions: {
-            level: "error"
-        }
+
+        reporters: ['progress', 'junit', 'coverage'],
+
+        preprocessors: {
+            // Files that should be included or excluded from coverage analysis
+            'webapp/*.js': ['coverage'],
+            'webapp/!(test|localService)/**/*.js': ['coverage']
+        },
+
+        coverageReporter: {
+            type: 'cobertura',
+            dir: 'coverage/',
+            file: 'cobertura.xml'
+        },
+
+        browsers: ["ChromeHeadless"],
+
+        singleRun: true,
+
+        failOnEmptyTestSuite: false
     });
 };
